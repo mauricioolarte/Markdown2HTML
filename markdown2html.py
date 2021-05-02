df15d2eeb659fc96a0d4d825786b5c031fcf697c
mauricioolarte <reneolarte@yahoo.com>
2021-05-02 00:20:34 -0500

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+''' esta fucion es para hacer marckdown'''
+
 import sys
+import re
+import hashlib
 
 linestosave = []
 flag = ''
@@ -16,6 +20,7 @@ selectores = [
 
 
 def boldemphasis(text):
+    ''' bold '''
     bold = '**'
     emphasis = '__'
     if bold in text and text.count(bold) % 2 == 0:
@@ -38,28 +43,33 @@ def boldemphasis(text):
 
 
 def saveLine(list, destinyfile):
-    with open(destinyfile, 'a') as f:
+    ''' save in new file '''
+    with open(destinyfile, 'w') as f:
         for line in list:
             f.write(line)
 
 
 def heading(text):
+    ''' put h tags '''
     flag = 'heading'
     typeHeader = str(text[0].count('#'))
     opentag = '<h' + typeHeader + '>'
     closetag = '</h' + typeHeader + '>'
     linewhitboldoremphasis = boldemphasis(text[1])
+    linewhitboldoremphasis = butwhy(linewhitboldoremphasis)
     linetosave = opentag + \
         linewhitboldoremphasis.replace('\n', '') + closetag + '\n'
     linestosave.append(linetosave)
 
 
 def unorderedlisting(text):
+    ''' put ul tags '''
     global flag
     actualflag = 'ul'
     opentag = '<ul>\n'
     closetag = '</ul>\n'
     linewhitboldoremphasis = boldemphasis(text[1])
+    linewhitboldoremphasis = butwhy(linewhitboldoremphasis)
     linetosave = '<li>' + linewhitboldoremphasis.replace('\n', '') + '</li>\n'
     if flag == actualflag:
         linestosave.pop()
@@ -68,16 +78,17 @@ def unorderedlisting(text):
         linestosave.append(opentag)
         linestosave.append(linetosave)
     linestosave.append(closetag)
-
     flag = 'ul'
 
 
 def orderedlisting(text):
+    ''' put ol tags '''
     global flag
     actualflag = 'ol'
     opentag = '<ol>\n'
     closetag = '</ol>\n'
     linewhitboldoremphasis = boldemphasis(text[1])
+    linewhitboldoremphasis = butwhy(linewhitboldoremphasis)
     linetosave = '<li>' + linewhitboldoremphasis.replace('\n', '') + '</li>\n'
     if flag == actualflag:
         linestosave.pop()
@@ -86,16 +97,17 @@ def orderedlisting(text):
         linestosave.append(opentag)
         linestosave.append(linetosave)
     linestosave.append(closetag)
-
     flag = 'ol'
 
 
 def simpletex(text):
+    ''' put bold and emphasis tags '''
     global flag
     actualflag = 'text'
     opentag = '<p>\n'
     closetag = '</p>\n'
     linewhitboldoremphasis = boldemphasis(text)
+    linewhitboldoremphasis = butwhy(linewhitboldoremphasis)
     linetosave = linewhitboldoremphasis.replace('\n', '') + '\n'
     if flag == actualflag:
         linestosave.pop()
@@ -106,6 +118,27 @@ def simpletex(text):
         linestosave.append(linetosave)
     linestosave.append(closetag)
     flag = 'text'
+
+
+def butwhy(text):
+    ''' made weird things '''
+    translations = {}
+    coincidencias = re.findall("\[\[.*\]\]|\(\(.*\)\)", text)
+    for coincidencia in coincidencias:
+        if coincidencia.find('[[') >= 0 or coincidencia.find(']]') >= 0:
+            value = coincidencia
+            for simbol in ['[[', ']]']:
+                value = value.replace(simbol, '')
+                translations[coincidencia] = hashlib.md5(
+                    value.encode()).hexdigest()
+        if coincidencia.find('((') >= 0 or coincidencia.find('))') >= 0:
+            value = coincidencia
+            for simbol in ['C', 'c', '(', ')']:
+                value = value.replace(simbol, '')
+                translations[coincidencia] = value
+    for translation in translations:
+        text = text.replace(translation, translations[translation])
+    return (text)
 
 
 myfunctions = {
@@ -120,12 +153,7 @@ def operation(name, listline):
 
 
 if __name__ == "__main__":
-    print('hola')
-
     arguments = sys.argv
-
-    print(arguments)
-    print(len(arguments))
 
     if len(arguments) < 3:
         sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
@@ -133,10 +161,8 @@ if __name__ == "__main__":
 
     try:
         fileToRead = arguments[1]
-        print(fileToRead)
         with open(fileToRead, 'r') as f:
             lista = list(f)
-            print(lista)
             for line in lista:
                 splitline = line.split(' ', 1)
                 if splitline[0] == '\n':
@@ -150,5 +176,6 @@ if __name__ == "__main__":
         flag = ''
         sys.exit(0)
     except OSError:
-        sys.stderr.write('Missing <filename>\n')
+        message = 'Missing ' + arguments[1] + '\n'
+        sys.stderr.write(message)
         sys.exit(1)
